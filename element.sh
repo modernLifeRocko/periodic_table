131ef79d0f654eb9bd6c9c475b554592c00b0f3d
modernLifeRocko <affontalvo@gmail.com>
2023-09-19 05:25:11 +0000

    feat:add print from symbol or name

--- element.sh
+++ element.sh
@@ -9,7 +9,7 @@ PRINT_FROM_NUMBER () {
 	then
 		echo "I could not find that element in the database."
 	else
-		echo "The element with atomic number $1 is '$NAME' ($SYMBOL). It's a '$TYPE', with a mass of $ATOMIC_MASS amu. '$NAME' has a melting point of of $MELTING_POINT celsius and a boiling point of $BOILING POINT celsius."
+		echo "The element with atomic number $1 is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
 	fi
 	done
 }
@@ -20,9 +20,13 @@ elif [[ $1 =~ ^[0-9]+$ ]]
 then
 	PRINT_FROM_NUMBER $1
 else
-	echo "else"
 	#get atomic weight
+	ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1' OR name='$1'")
 	#if not exist
-		# exit
-	#else print info
+	if [[ -z $ATOMIC_NUMBER ]]
+	then
+		echo "I could not find that element in the database."
+	else
+		PRINT_FROM_NUMBER $ATOMIC_NUMBER
+	fi
 fi
