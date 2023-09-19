8913a4bf3b7c970b582d49b812d3a9b9ed50df22
modernLifeRocko <affontalvo@gmail.com>
2023-09-19 03:17:33 +0000

    feat:add response to empty case

--- element.sh
+++ element.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+# get info from periodic table 
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+if [[ -z $1 ]]
+then
+	echo "Please provide an element as an argument."
+elif [[ $1 =~ ^[0-9]+$ ]] 
+then
+	echo "number"
+	#if not in table
+		# exit
+	#else print info
+else
+	echo "else"
+	#get atomic weight
+	#if not exist
+		# exit
+	#else print info
+fi
